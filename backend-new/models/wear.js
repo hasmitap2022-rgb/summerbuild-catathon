0182f2249573b80c8a0afc8cc8218618afd85e62
vibushaharibaskar-arch <vibushaharibaskar@gmail.com>
2026-06-15 12:27:07 +0800

    Update wear.js

--- backend-new/models/wear.js
+++ backend-new/models/wear.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const wearSchema = new mongoose.Schema({
+  userId: String,
   itemId: String,
   itemName: String,
   wornOn: { type: Date, default: Date.now }
